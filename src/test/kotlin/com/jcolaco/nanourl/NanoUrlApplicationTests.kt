1c2f5acaef9f4c1d87d5a3d830fa4ded8c11a4a1
Joao Colaco <joao.colaco@talkdesk.com>
2020-04-25 21:06:59 +0100

    Initial Commit

---
+++ src/test/kotlin/com/jcolaco/nanourl/NanoUrlApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.jcolaco.nanourl
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class NanoUrlApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
